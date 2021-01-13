c4b049c6cb8b6d7586864a51e4223469524f8165
Mingming Caressi <caressi.m@gmail.com>
2021-01-12 23:35:13 -0800

    Delete models directory

--- models/muscles/seeds.sql
+++
@@ -1,8 +0,0 @@
-USE muscles_db;
-
-INSERT INTO muscles (name, category, is_front) VALUES 
-("Biceps brachii (Arms)", "Arms", 1), ("Anterior deltoid", "Shoulders", 1), ("Serratus anterior", "Chest", 1),
-("Pectoralis major", "Chest", 1), ("Triceps brachii", "Arms", 0), ("Rectus abdominis", "Abs", 1),
-("Gastrocnemius (Calves)", "Calves", 0), ("Gluteus maximus", "Glutes", 1), ("Trapezius", "Back", 0), 
-("Quadriceps femoris", "Legs", 1), ("Biceps femoris (Thigh)", "Legs", 0), ("Latissimus dorsi", "Back", 0),
-("Brachialis (Tricep)", "Arms", 1), ("Obliquus externus abdominis", "Abs", 1), ("Soleus (Calves)", "Calves", 0);
